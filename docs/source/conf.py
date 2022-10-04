44b5c5d2de0a2b7521b8775ee748bcc77613fe8b
Sungkeun Kim <sungkeun.kim@icloud.com>
2022-10-04 08:16:21 -0500

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Texas A&M University CSCE 312 Term project tutorials'
+copyright = '2022, Sungkeun Kim'
+author = 'Sungkeun Kim'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
